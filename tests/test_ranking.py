3886d30c3dec9531e6085d0e632ad2d3835f6241
roger <rogerortiz4@gmail.com>
2022-09-06 13:32:20 -0500

    adding negative test cases

--- tests/test_ranking.py
+++ tests/test_ranking.py
@@ -1,7 +1,16 @@
 import json
 import pytest
 from typer.testing import CliRunner
-from ranking import __app_name__, __version__, cli, DB_READ_ERROR, SUCCESS, ranking
+from ranking import (
+    MISSING_TEAM_ERROR,
+    NO_SCORE_ERROR,
+    SAME_TEAM_ERROR,
+    SUCCESS,
+    __app_name__,
+    __version__,
+    cli,
+    ranking,
+)
 
 runner = CliRunner()
 
@@ -44,29 +53,68 @@ def mock_matches_json(tmp_path):
     return db_file
 
 
-test_matches_1 = {
-    "match_1": "Lions 3, Snakes 3",
-    "match": {
+test_match_1 = {
+    "match": "Lions 3, Snakes 3",
+    "teams": {
         "team_1": {"name": "Lions", "goals": 3},
         "team_2": {"name": "Snakes", "goals": 3},
     },
 }
 
+test_match_2 = {
+    "match": "Lions 1,",
+    "error": "Lions 1,",
+}
+
+test_match_3 = {
+    "match": "Lions aa, Giants 4",
+    "error": "Lions aa, Giants 4",
+}
+
+test_match_4 = {
+    "match": "Lions 1, Lions 1",
+    "error": "Lions 1, Lions 1",
+}
+
 
 @pytest.mark.parametrize(
-    "match_1, expected",
+    "match, expected",
     [
         pytest.param(
-            test_matches_1["match_1"],
-            (test_matches_1["match"], SUCCESS),
-        )
+            test_match_1["match"],
+            (test_match_1["teams"], SUCCESS),
+        ),
     ],
 )
-def test_add(mock_matches_json, match_1, expected):
+def test_add(mock_matches_json, match, expected):
     ranking_controller = ranking.RankingController(mock_matches_json)
-    assert ranking_controller.add(match_1) == expected
+    assert ranking_controller.add(match) == expected
     read = ranking_controller._db_handler.read_matches()
     assert len(read.matches_list) == 6
+
+
+@pytest.mark.parametrize(
+    "match, expected",
+    [
+        pytest.param(
+            test_match_2["match"],
+            (test_match_2["error"], MISSING_TEAM_ERROR),
+        ),
+        pytest.param(
+            test_match_3["match"],
+            (test_match_3["error"], NO_SCORE_ERROR),
+        ),
+        pytest.param(
+            test_match_4["match"],
+            (test_match_4["error"], SAME_TEAM_ERROR),
+        ),
+    ],
+)
+def test_add_failures(mock_matches_json, match, expected):
+    ranking_controller = ranking.RankingController(mock_matches_json)
+    assert ranking_controller.add(match) == expected
+    read = ranking_controller._db_handler.read_matches()
+    assert len(read.matches_list) == 5
 
 
 def test_show_all_matches(mock_matches_json):
@@ -79,9 +127,9 @@ def test_show_all_matches(mock_matches_json):
 def test_show_table_ranking(mock_matches_json):
     ranking_controller = ranking.RankingController(mock_matches_json)
     table_rank, error = ranking_controller.show_table_ranking()
-    expected = ranking.CurrentRank(
+    table_expected, error_expected = ranking.CurrentRank(
         {"tarantulas": 6, "lions": 5, "FC awesome": 1, "snakes": 1, "grouches": 0},
         SUCCESS,
     )
-    assert table_rank == expected.table
-    assert error == expected.error
+    assert table_rank == table_expected
+    assert error == error_expected
